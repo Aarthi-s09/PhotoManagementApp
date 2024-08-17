19f91fde73cd044ed52f1d381111f9c8167a0836
Aarthi-s09 <123437754+Aarthi-s09@users.noreply.github.com>
2024-08-17 20:48:05 +0530

    Update UploadPage.js

--- frontend/src/UploadPage.js
+++ frontend/src/UploadPage.js
@@ -23,7 +23,7 @@ function UploadPage({ addPhoto }) {
     formData.append('description', description);
 
     try {
-      const response = await fetch('http://localhost:5000/upload', {
+      const response = await fetch('https://photobackend-hhgk.onrender.com/upload', {
         method: 'POST',
         body: formData,
       });
